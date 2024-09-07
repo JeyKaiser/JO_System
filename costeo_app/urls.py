1389d3450f360742f6ca9e174431e9b455352500
JeyKaiser <jeykokaiser2023@gmail.com>
2024-09-07 11:06:07 -0500

    comentario para probar clonado de repo

--- costeo_app/urls.py
+++ costeo_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 
+#COMENTARIO DE PUEBA PARA CREAR UN COMMIT
 urlpatterns = [
     path('', views.hello),
     path('about/', views.about),
