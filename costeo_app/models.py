6788269e088579bcffb6eb82d232b553b3e55519
JeyKaiser <jeykokaiser2023@gmail.com>
2024-09-07 15:57:19 -0500

    creacion modelos Proyecto y Tasks

--- costeo_app/models.py
+++ costeo_app/models.py
@@ -4,9 +4,17 @@ from django.db import models
 
 class Aparrel(models.Model):
     id = models.AutoField(primary_key=True)
-    referencia = models.CharField(max_length=30, null=True, blank=True)
+    referencia = models.IntegerField(max_length=30, null=True, blank=True)
     fotoReferencia = models.CharField(max_length=30, null=True, blank=True)
     codigoSapMD = models.CharField(max_length=30, null=True, blank=True)
     codigoSapPT = models.CharField(max_length=30, null=True, blank=True)
     nombreSistema = models.CharField(max_length=30, null=True, blank=True)
     descripcionColor = models.CharField(max_length=30, null=True, blank=True)
+
+class Proyecto(models.Model):
+    nombre_proyecto = models.CharField(max_length=50)
+
+class Tasks(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
